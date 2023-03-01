a4c346bc047f7c42fbdeb9599017364b06d07f1b
georgesxo <awuahg500@gmmail.com>
2023-03-01 08:33:00 -0800

    5-string_toupper.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,14 +2,14 @@
 /**
 * positive_or_negative - checks for positive or negative numbers
 *
-* @i: the number to be  checked
+* @i: the number to be checked
 *
 * Return: always 0
 */
 void positive_or_negative(int i)
 {
-if (i < 0)
-printf("%d is negative\n", i);
-else
-printf("%d positive\n", i);
+	if (i < 0)
+	printf("%d is negative\n", i);
+	else
+	printf("%d positive\n", i);
 }
