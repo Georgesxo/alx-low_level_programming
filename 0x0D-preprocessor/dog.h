86a8672a13a6004932907c3bdd316de20de0f2aa
georgesxo <awuahg500@gmmail.com>
2023-03-23 05:29:24 -0700

    function_pointers.h

---
+++ 0x0D-preprocessor/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - a dog's basic info
+* @breed: First member
+* @dog_t: my dog;
+* @owner: Third member
+* @name: fourth member
+* @age: fiveth member
+*
+* Description: Longer description
+*/
+typedef struct dog
+{
+char name[50];
+int age;
+char breed[50];
+dog_t;
+}
+#endif
